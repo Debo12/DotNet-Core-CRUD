ebcc9ac94a7c4ac66d51282a776f7747299f54db
Debo12 <debojyotidutta94@gmail.com>
2019-08-27 01:08:07 +0530

    CRUD Operaion done

---
+++ CoreCRUD/CoreCRUD/Models/Employee.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace CoreCRUD.Models
+{
+    public class Employee
+    {
+        [Key]
+        public int EmployeeID { get; set; }
+        [Required]
+        [Column(TypeName ="varchar(250)")]
+        public string EmployeeName { get; set; }
+        [Column(TypeName = "varchar(10)")]
+        public string EmployeeCode { get; set; }
+        [Column(TypeName = "varchar(100)")]
+        public string Position { get; set; }
+        [Column(TypeName = "varchar(100)")]
+        public string OfficeLocation { get; set; }
+    }
+}
